afc82c6d6ad984b2b81fd98a7de877ef32a14759
Angel Piscola <angelpiscola@gmail.com>
2017-12-02 16:51:44 -0500

    Make spawner, gamestate, and enemy behavior

---
+++ Assets/ScriptCode/GameState.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameState : MonoBehaviour
+{
+
+	public static int difficulty;
+	// Use this for initialization
+	void Start ()
+	{
+		difficulty = 0;
+		InvokeRepeating ("IncreaseDifficulty", 0, 5);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+
+	void IncreaseDifficulty() {
+		difficulty++;
+	}
+}
+
