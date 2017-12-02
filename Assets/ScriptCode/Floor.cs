43d6e2e22d434a502024ffbc6e465009d1912df2
Jose Mercado <jmercado101@hotmail.com>
2017-12-02 14:24:07 -0500

    initial command

---
+++ Assets/ScriptCode/Floor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Floor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.tag == "MelPlayer")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
---
+++ Assets/ScriptCode/MelMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MelMovement : MonoBehaviour {
+
+    public float speed = 1;
+    public int score = 0;
+    public GameObject gameOver;
+    public Text userScore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Movement up
+		if (Input.GetKey(KeyCode.UpArrow)&&(transform.position.y<2.20))
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+
+        //Movement down
+        if (Input.GetKey(KeyCode.DownArrow) && (transform.position.y > -2.20))
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+
+        //Right movement
+        if (Input.GetKey(KeyCode.RightArrow) && (transform.position.x < 2.20))
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        //Left movement
+        if (Input.GetKey(KeyCode.LeftArrow) && (transform.position.x > -2.20))
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+	}
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "FloorTest")
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
