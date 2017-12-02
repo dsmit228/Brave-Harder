36fbce03bf0565d7b24b29a7a514962e5297b8b4
Angel Piscola <angelpiscola@gmail.com>
2017-12-02 16:51:44 -0500

    Make spawner, gamestate, and enemy behavior

---
+++ Assets/ScriptCode/EnemyBehavior.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehavior : MonoBehaviour
+{
+	float fallSpeed = 8.0f;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.Translate(Vector2.down * fallSpeed * Time.deltaTime, Space.World);
+	}
+
+	void OnBecameInvisible() {
+		Destroy (gameObject);
+	}
+}
+
